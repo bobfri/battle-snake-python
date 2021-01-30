51b7fd152e21f47aa414ca73506ba2953758c75e
bobfri <ch.c@live.ca>
2021-01-30 23:53:10 +0000

    up to date

--- server.py
+++ server.py
@@ -18,8 +18,6 @@ class Board(object):
       
       return
 
-
-    #TODO clean this
     def check(self,move,head,length):
       moves_ressult = {
           "up":{"x":0,"y":1},
@@ -135,7 +133,7 @@ class Battlesnake(object):
           print(e)
           print("random")
 
-          #move = random.choice(possible_moves)
+          move = random.choice(possible_moves)
         print(f"MOVE: {move}")
         return {"move": move}
 
